89e4df1623be752a7c23a82ff9167b77e3a9fd34
Francis Labelle <stashingpixels@gmail.com>
2016-05-09 16:35:52 -0400

    Add main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.columnconfigure(0, weight = 1)
+    root.rowconfigure(0, weight = 1)
+    root.title("Stream Downloader")
+
+    x = 400
+    y = 200
+    w = 400
+    h = 200
+
+    root.geometry("%dx%d+%d+%d" % (w, h, x, y))
+
+    root.mainloop()
