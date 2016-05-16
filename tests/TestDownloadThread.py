1936f237d70e53134b16ce69d14ac8329fe0238c
Francis Labelle <stashingpixels@gmail.com>
2016-05-15 23:14:57 -0400

    Fix invalid attribute in test

--- tests/TestDownloadThread.py
+++ tests/TestDownloadThread.py
@@ -15,10 +15,10 @@ class MockStreamFile:
         self.position = 0
         self.size = size
 
-    def read(self, bufferSize):
-        previousPosition = self.position
-        self.position = min(self.position + bufferSize, self.size)
-        return "a" * (self.position - previousPosition)
+    def read(self, buffer_size):
+        previous_position = self.position
+        self.position = min(self.position + buffer_size, self.size)
+        return "a" * (self.position - previous_position)
 
     def close(self):
         pass
@@ -41,7 +41,7 @@ class TestDownloadThread(unittest.TestCase):
         self.thread = thread.DownloadThread(self.stream, self.file)
 
     def test_starting_size(self):
-        self.assertEqual(0, self.thread.totalSize)
+        self.assertEqual(0, self.thread.total_size)
 
     def test_terminates(self):
         self.thread.start()
@@ -61,9 +61,9 @@ class TestDownloadThread(unittest.TestCase):
         self.thread.join(1)
         self.assertTrue(self.thread.is_alive())
 
-        currentSize = self.file.size
+        current_size = self.file.size
         self.thread.join(1)
-        self.assertEqual(currentSize, self.file.size)
+        self.assertEqual(current_size, self.file.size)
 
         self.thread.resume()
         self.thread.join(self.TIMEOUT)
